f04b32e3a8603d7097175e81c64f74fdcdfb6012
DanielsLuz <danielluz1995@gmail.com>
2016-09-01 21:19:13 -0300

    Refactored for readability.

--- code/Pico/Testes.hs
+++ code/Pico/Testes.hs
@@ -24,19 +24,19 @@ y50 = Assignment "y" (ExpValue (NATValue 50))
 
 -- | Simple sum statement test
 sumProgram :: Program
-sumProgram = Program st [x10, y50, res1]
+sumProgram = Program st [x10, y50, resultSum]
 
-res1 :: Statement
-res1 = Assignment "output" (Add (Var "x") (Var "y"))
+resultSum :: Statement
+resultSum = Assignment "output" (Add (Var "x") (Var "y"))
 
 sumTest = TestCase (assertEqual "for x = 10; y = 50; output = x + y;" (NATValue 60) (runProgram sumProgram)) 
 
 -- | Simple subtraction statement test
 subProgram :: Program
-subProgram = Program st [x10, y50, res2] 
+subProgram = Program st [x10, y50, resultSub] 
 
-res2 :: Statement
-res2 = Assignment "output" (Sub (Var "y") (Var "x"))
+resultSub :: Statement
+resultSub = Assignment "output" (Sub (Var "y") (Var "x"))
 
 subTest = TestCase (assertEqual "for x = 10; y = 50; output = y - x;" (NATValue 40) (runProgram subProgram))
 
@@ -48,15 +48,13 @@ out10 :: Statement
 out10 = Assignment "output" (ExpValue (NATValue 10))
 
 ifBlock :: Statement
-ifBlock = IfThen ifExp ifBody
-
-ifExp :: Expression
-ifExp = (ExpValue (NATValue 1))
+ifBlock = IfThen trueExp ifBody
 
 ifBody :: Statement
 ifBody = Assignment "output" (ExpValue (NATValue 20))
 
-ifThenTest = TestCase (assertEqual "for output = 10; if(1){ output = 20 }" (NATValue 20) (runProgram ifThenProgram)) 
+ifThenTest = TestCase (assertEqual "for output = 10; if(1){ output = 20 }" 
+                      (NATValue 20) (runProgram ifThenProgram)) 
 
 -- | Simple ifThenElse statement test
 ifThenElseProgram1 :: Program
@@ -65,17 +63,11 @@ ifThenElseProgram1 = Program st [ifElseBlock1]
 ifElseBlock1 :: Statement
 ifElseBlock1 = IfThenElse trueExp statement1 statement2
 
-trueExp :: Expression
-trueExp = (ExpValue (NATValue 1))
-
 ifThenElseProgram2 :: Program
 ifThenElseProgram2 = Program st [ifElseBlock2]
 
 ifElseBlock2 :: Statement
 ifElseBlock2 = IfThenElse falseExp statement1 statement2
-
-falseExp :: Expression
-falseExp = (ExpValue (NATValue 0))
 
 statement1 :: Statement
 statement1 = Assignment "output" (ExpValue (NATValue 1))
@@ -83,7 +75,19 @@ statement1 = Assignment "output" (ExpValue (NATValue 1))
 statement2 :: Statement
 statement2 = Assignment "output" (ExpValue (NATValue 2))
 
+-- Test for Then clause
 ifThenElseTest1 = TestCase (assertEqual "if(1){output = 1} else {output = 2}" 
-                             (NATValue 1) (runProgram ifThenElseProgram1)) 
+                           (NATValue 1) (runProgram ifThenElseProgram1)) 
+-- Test for Else clause
 ifThenElseTest2 = TestCase (assertEqual "if(0){output = 1} else {output = 2}" 
-                             (NATValue 2) (runProgram ifThenElseProgram2)) 
+                           (NATValue 2) (runProgram ifThenElseProgram2)) 
+
+-- | Utility functions for tests
+
+-- | True expression
+trueExp :: Expression
+trueExp = (ExpValue (NATValue 1))
+
+-- | False expression
+falseExp :: Expression
+falseExp = (ExpValue (NATValue 0))
