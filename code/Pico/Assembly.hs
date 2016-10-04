c8ed423358f261f4816ad06b5ac43a69fbb28854
Diego Marcilio <diego.marcilio@rhases.com.br>
2016-10-04 01:10:50 -0300

    Fixing our assembly definition. We use 'Ident' instead of 'Name'.
    - Not sure if Ident should be imported from AbsPico.

--- code/Pico/Assembly.hs
+++ code/Pico/Assembly.hs
@@ -1,7 +1,9 @@
 module Pico.Assembly where
 
+-- Ident should be imported from AbsPico?
+import Pico.AbsPico
+
 type Label = String
-type Name = String
 
 -- TODO: define instructions for expressions:
 ---- GTE
@@ -11,12 +13,12 @@ type Name = String
 ---- Pow
 
 data Instr
-    = DclInt Name -- Reserve a memory location for an integer variable
-    | DclStr Name -- Reserve a memory location for a string variable
+    = DclInt Ident -- Reserve a memory location for an integer variable
+    | DclStr Ident -- Reserve a memory location for a string variable
     | PushNat Int -- Push integer constant on the stack
     | PushStr String -- Push string constant on the stack
-    | Rvalue Name -- Push the value of a variable on the stack
-    | Lvalue Name -- Push the address of a variable on the stack
+    | Rvalue Ident -- Push the value of a variable on the stack
+    | Lvalue Ident -- Push the address of a variable on the stack
     | AssignOp -- Assign value on top, to variable at address top-1
     | AddOp -- Replace top two stack values by their sum
     | SubOp -- Replace top two stack values by their difference
